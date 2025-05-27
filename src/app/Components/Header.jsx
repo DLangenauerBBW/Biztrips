87ea5851dd4c9d6ff1cde0ddef315091e07f38c6
DLangenauerBBW <Dominic.langenauer@lernende.bbw.ch>
2025-05-27 15:11:15 +0200

    Coverpage

---
+++ src/app/Components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+            <img width="150px" alt="Business Trips" src="/images/logo.png" />
+      </nav>
+    </header>
+  );
+}
---
+++ src/app/booking/page.js
@@ -0,0 +1,10 @@
+import styles from "./booking.module.css";
+
+export default function page() {
+
+  return (
+    <div className={styles.container}>
+        <h1>Ihre Anlaufstelle für Geschäftsreisen</h1>
+    </div>
+  );
+}
